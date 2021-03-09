c7c7b561526ca22b5dfa5117dc9a2432aabe8051
Dinmukhamed <s.dinmuhamed@gmail.com>
2021-03-09 23:00:35 +0600

    fix: fix min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length < 1)
+    return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length < 1)
+    return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length < 1)
+    return 0;
+
+  const sum = array.reduce((acc, curr) => acc + curr, 0);
+  return sum / array.length;
 }
